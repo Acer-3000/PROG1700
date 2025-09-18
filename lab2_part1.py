fe5dda0f6cca998f2f9f381ccf305d8c9b81a36c
Colin Sample <colinsample3000@gmail.com>
2025-09-18 14:23:09 -0300

    update

---
+++ lab2_part1.py
@@ -0,0 +1,13 @@
+name = "Colin"
+age = 29
+height = 1.64592   # in meters
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(Student)
+
+print(type(name))
+print(type(age))
+print(type(height))
