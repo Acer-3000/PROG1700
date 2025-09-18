1fe2d9ae5b3cfc9a7331c8d554f538768811bb7f
Colin Sample <colinsample3000@gmail.com>
2025-09-18 16:17:22 -0300

    save

--- lab3/lab3_part3.py
+++ lab3/lab3_part3.py
@@ -1,6 +1,7 @@
 temp = float(input("Enter the temperature (°C):"))
+raining = input("is it raining y/n")
 
-if temp < 0 or temp > 35: 
+if temp < 0 and raining == "y":
     print("Warning: Extreme temperature!")
 else:
     print("Temperature is normal.")
