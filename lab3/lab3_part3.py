ce45b3e9eb3015194d81e1692189bdc242458dd5
Colin Sample <colinsample3000@gmail.com>
2025-09-18 15:52:02 -0300

    save

--- lab3/lab3_part3.py
+++ lab3/lab3_part3.py
@@ -1,6 +1,6 @@
-temp = float(input("Enter the temperature (°C): "))
+temp = float(input("Enter the temperature (°C):"))
 
-if temp < 0 or temp > 35:
+if temp < 0 or temp > 35: 
     print("Warning: Extreme temperature!")
 else:
     print("Temperature is normal.")
---
+++ lab3/lab3_part4.py
@@ -0,0 +1,10 @@
+user = input("Enter username: ")
+password = input("Enter password: ")
+
+if user == "admin":
+    if password == "1234":
+        print("Access granted.")
+    else:
+        print("Wrong password.")
+else:
+    print("Unknown user.")
