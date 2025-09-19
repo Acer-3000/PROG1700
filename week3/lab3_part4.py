77d3a22f03c874271ff8ac1f40cf02de12c417e9
Colin Sample <colinsample3000@gmail.com>
2025-09-19 11:24:49 -0300

    name changes

---
+++ week3/lab3_part4.py
@@ -0,0 +1,16 @@
+user = input("Enter username: ")
+password = input("Enter password: ")
+
+if user == "Admin":
+        if password == "1234":
+            print("Access granted.")
+        else:
+            print("Wrong password.")
+if user == "student":
+        if password == "4321":
+            print("Access granted.")
+        else:
+            print("Wrong password.")
+else:
+    print("Unknown user") 
+    
---
+++ week3/lab3_part5.py
@@ -0,0 +1,9 @@
+x = 7
+y = 3
+
+if x > 5 and y < 5:
+    print("Case 1")
+elif x < 5:
+    print("Case 2")
+else:
+    print("Case 3")
