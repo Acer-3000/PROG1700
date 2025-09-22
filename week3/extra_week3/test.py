0dc9bbd791718c1afd7d23514a2a1436283e5f22
Colin Sample <colinsample3000@gmail.com>
2025-09-22 12:19:44 -0300

    class update

--- week3/extra_week3/test.py
+++ week3/extra_week3/test.py
@@ -1,22 +1,9 @@
-card = input("Insert card (yes): ")
-while attempts < 1 and card == 'yes':
-    card = input("Insert card (yes): ")
-if card == 'yes':
-    print("Card accepted")
-    
 pin = input("Enter PIN: ") 
 pin = True if pin == "4321" else False
+if False 
 
-print("Access granted" if pin else "Wrong PIN")
-attempts = 0 
-if pin:
-    while attempts < 1:
-        pin = input("Enter PIN: ") 
-    pin = True if pin == "4321" else False
-    
-    print("Access granted" if pin else "Wrong PIN")
-    attempts += 1
-if pin == True:
+
+if True:
     options = input("Balance, Withdraw, Deposit: ")
     if options == "Balance":
         print("Your balance is $500")
