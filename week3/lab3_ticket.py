9afc37bea71a0220486a9cd7d674114d975491e5
Colin Sample <colinsample3000@gmail.com>
2025-09-23 15:15:13 -0300

    fix

--- week3/lab3_ticket.py
+++ week3/lab3_ticket.py
@@ -1,9 +1,9 @@
-age = input("Age in years")
+age = int(input("Age in years"))
 if age < 5:
     print("Free Ticket")
 elif age >= 5 and age <= 12:
     print("Fee is $8")
-elif age >= 13 and age <= 65:
+elif age >= 13 and age <= 64:
     print("Fee is $12")
 else:
     Print("Fee is $6")
