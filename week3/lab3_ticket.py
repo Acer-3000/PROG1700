879dd71c6074a3aa6b664f37fe4e919ef229ac42
Colin Sample <colinsample3000@gmail.com>
2025-09-23 13:51:12 -0300

    small changes

--- week3/lab3_ticket.py
+++ week3/lab3_ticket.py
@@ -1,8 +1,8 @@
-input("age")
+age = input("age")
 input("under 13")
 
-age < 5 ticket = 0
-age > 5 ticket = 8
-age > 13 ticket = 12
-age > 65 ticket = 6
-under 13 =
+age < 5 = ticket = 0
+age = (6-12) = ticket = 8
+age > (13-64) = ticket = 12
+age > 65 = ticket = 6
+age = (13-6) = ticket =  
